209dfa9c671fa89c9c428d86f99f320c09a47e0e
Hendrik and Camila <hendrikandcamila@hendriks-mbp.lan>
2021-08-02 14:37:19 -0500

    starter new and fresh

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === `production` && config.siteUrl === `http://localhos
 */
 module.exports = {
     siteMetadata: {
-        siteUrl: "selfgrowthguru.com" || process.env.SITEURL || config.siteUrl,
+        siteUrl: "https://selfgrowthguru.com" || process.env.SITEURL || config.siteUrl,
     },
     plugins: [
         /**
